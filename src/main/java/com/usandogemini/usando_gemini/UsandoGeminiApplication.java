c80378380573a1a2467e3d807d5fe3d67e37559e
aherreramilet <aherrera@dl.cl>
2025-03-09 10:24:22 -0300

    update project

---
+++ src/main/java/com/usandogemini/usando_gemini/UsandoGeminiApplication.java
@@ -0,0 +1,13 @@
+package com.usandogemini.usando_gemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsandoGeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsandoGeminiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usandogemini/usando_gemini/UsandoGeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usandogemini.usando_gemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsandoGeminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
